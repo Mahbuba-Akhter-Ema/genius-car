61d9657ab8de91d0a7ddae3d571e5d7f2ae1d7bf
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2023-03-30 17:39:25 -0400

    add a banner for dream job

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main/Main";
 import CheckOut from "../../Pages/CheckOut/CheckOut";
+import DreamJob from "../../Pages/DreamJob/DreamJob";
 import Home from "../../Pages/Home/Home/Home";
 import LogIn from "../../Pages/LogIn/LogIn";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <SignUp></SignUp>
+            },
+            {
+                path: '/dreamjob',
+                element: <DreamJob></DreamJob>
             },
             {
                 path: '/checkout/:id',
